6caa42276f8b5d8cd97d63a4cb79855feaf84750
Sunil Raut <sunil.rautkshetri@gmail.com>
2020-08-15 08:25:50 +0200

    Implement TestRemoveHeadFunction() function in TestSinglyLinkList.cpp

--- SinglyLinkList/TestSinglyLinkList.cpp
+++ SinglyLinkList/TestSinglyLinkList.cpp
@@ -30,3 +30,26 @@ void TestInsertHeadFunction() {
 	cout << "\n-----------------------------\n";
 
 }
+
+void TestRemoveHeadFunction() {
+	List<int> l{};
+	assert(l.IsEmpty());
+	assert(l.CountNode() == 0);
+	cout << "Node count = " << l.CountNode() << '\n';
+	l.InsertHead(1);
+	l.InsertHead(2);
+	l.InsertHead(3);
+	assert(!l.IsEmpty());
+	assert(l.CountNode() == 3);
+	cout << "l.InsertHead(1) \n";
+	cout << "l.InsertHead(2) \n";
+	cout << "l.InsertHead(3) \n";
+	cout << "Node count = " << l.CountNode() << '\n';
+	l.RemoveHead();
+	assert(l.CountNode() == 2);
+	cout << "l.RemoveHead() \n";
+	cout << "Node count = " << l.CountNode() << '\n';
+	cout << "\n-----------------------------\n";
+	cout << "RemoveHead() Test PASSED!";
+	cout << "\n-----------------------------\n";
+}
