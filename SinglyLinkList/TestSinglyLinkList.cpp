36592aaefda0a80616128e02b93b2babf4679dbe
Sunil Raut <sunil.rautkshetri@gmail.com>
2020-08-15 08:05:44 +0200

    Implement TestInsertHeadFunction() function in TestSinglyLinkList.cpp

--- SinglyLinkList/TestSinglyLinkList.cpp
+++ SinglyLinkList/TestSinglyLinkList.cpp
@@ -14,3 +14,18 @@ void TestEmptyList() {
 	cout << "EmptyList Test PASSED!";
 	cout << "\n-----------------------------\n";
 }
+
+void TestInsertHeadFunction() {
+	List<int> l{};
+	assert(l.IsEmpty());
+	assert(l.CountNode() == 0);
+	l.InsertHead(1);
+	assert(!l.IsEmpty());
+	assert(l.CountNode() == 1);
+	cout << "l.InsertHead(1) \n";
+	cout << "Node count = " << l.CountNode() << '\n';
+	cout << "\n-----------------------------\n";
+	cout << "InsertHead() Test PASSED!";
+	cout << "\n-----------------------------\n";
+
+}
